8a9aa1a975df2a3e4e1d9f2c70b5e0e139130af7
Natal Lima <nataldelima@gmail.com>
2024-04-29 20:04:05 -0400

    Bootcamp Aprendendo Sintaxe Java aulas 1-6

---
+++ tipos-variaveis/sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,34 @@
+public class Usuario {
+    public static void main(String[] args) {
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV ligada? " + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo status -> TV ligada? " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo status -> TV ligada? " + smartTv.ligada);
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        System.out.println("Canal atual: " + smartTv.canal);
+        smartTv.mudarCanal(13);
+        System.out.println("Canal atual: " + smartTv.canal);
+        smartTv.diminuirCanal();
+        smartTv.diminuirCanal();
+        System.out.println("Canal atual: " + smartTv.canal);
+        smartTv.aumentarCanal();
+        smartTv.aumentarCanal();
+        smartTv.aumentarCanal();
+        smartTv.aumentarCanal();
+        smartTv.aumentarCanal();
+        System.out.println("Canal atual: " + smartTv.canal);
+    }
+}
